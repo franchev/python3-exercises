5d8d44f808603ed645ce909b076923154ac1db85
dev-francesca <francesca@socure.be>
2021-08-25 13:14:17 -0400

    initial class created

---
+++ devoted_health_homework/InMemoryDb.py
@@ -0,0 +1,17 @@
+import logging
+import json
+import sys
+import collections
+
+
+class InMemoryDb:  
+    """
+    Class that Implement an in-memory database with the following functions:
+    - Reads values from STDIN line by line and executes functions as they happen
+    """
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
